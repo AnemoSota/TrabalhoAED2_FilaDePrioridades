a1ebfc5103012613da6d2a1d16e5839aabc48e21
Atos <atos.omena@icomp.ufam.edu.br>
2023-02-24 14:31:37 -0400

    modularizacao do arquivo de interfaces e correcao do main

--- Interface.py
+++ Interface.py
@@ -13,7 +13,6 @@ global l1
 global podeAlterar
 
 estadoAtual = list(range(9))
-#estadoAtual = [1,0,2,3,4,5,6,7,8,9]
 zero = busca_zero(estadoAtual)
 l1 = []
 podeAlterar = True
@@ -94,74 +93,71 @@ def atualiza():
     for i in range(9):
         l1[i].set(estadoAtual[i])
 
-janela= Tk()
+def telaStart():
+    janela= Tk()
 
 
-janela.title("Quebra-cabeças de 8 peças")
-janela.minsize(width=800, height=800)
-janela.maxsize(width=800, height=800)
+    janela.title("Quebra-cabeças de 8 peças")
+    janela.minsize(width=800, height=800)
+    janela.maxsize(width=800, height=800)
 
 
 
-font1=('Times',22,'normal')
-linha,coluna=0,0
-labels=[]
-indiceX=0.5
-indiceY=0.5
+    font1=('Times',22,'normal')
+    linha,coluna=0,0
+    indiceX=0.5
+    indiceY=0.5
 
-for i in range(9):
-    l1.append(StringVar())
-    l1[i].set(estadoAtual[i])
+    for i in range(9):
+        l1.append(StringVar())
+        l1[i].set(estadoAtual[i])
 
 
-for data in l1:
-    label=ttk.Label(janela,textvariable=data,font=font1, foreground= "red", background="white")
+    for data in l1:
+        label=ttk.Label(janela,textvariable=data,font=font1, foreground= "red", background="white")
+        
+        label.grid(row=linha,column=coluna,padx=50,pady=50)
+        coluna=coluna+1
     
-    label.grid(row=linha,column=coluna,padx=50,pady=50)
-    coluna=coluna+1
-  
-    label.place(relx = indiceX,rely = indiceY,anchor = 'center',x=-80,y=-80)
-    indiceX+=0.1
-    if coluna>=3:
-        indiceY+=0.1
-        linha+=1
-        coluna=0
-        indiceX=0.5
+        label.place(relx = indiceX,rely = indiceY,anchor = 'center',x=-80,y=-80)
+        indiceX+=0.1
+        if coluna>=3:
+            indiceY+=0.1
+            linha+=1
+            coluna=0
+            indiceX=0.5
 
 
-geraAleatoriosBotao= Button(janela, text="Gerar aleatorios",command= chamaAle)
-geraAleatoriosBotao.place(x=10,y=10)
+    geraAleatoriosBotao= Button(janela, text="Gerar aleatorios",command= chamaAle)
+    geraAleatoriosBotao.place(x=10,y=10)
 
-geraAleatoriosBotao.configure(height = 10,
-                             width = 20)
+    geraAleatoriosBotao.configure(height = 10, width = 20)
 
-Resolver= Button(janela, text="Resolver", command= chamaResolver)
-Resolver.place(x=640,y=10)
-Resolver.configure(height = 10,width = 20)
+    Resolver= Button(janela, text="Resolver", command= chamaResolver)
+    Resolver.place(x=640,y=10)
+    Resolver.configure(height = 10,width = 20)
 
 
-global laabel
-laabel=ttk.Label(janela, text="aaa", font=('Aerial 18'))
-laabel.place(relx = indiceX,rely = indiceY,anchor = 'center',x=-60,y=85)
+    global laabel
+    laabel=ttk.Label(janela, text="aaa", font=('Aerial 18'))
+    laabel.place(relx = indiceX,rely = indiceY,anchor = 'center',x=-60,y=85)
 
 
 
-Cima= Button(janela, text="↑", command= trocaUp)
-Cima.place(x=360,y=100)
-Cima.configure(height = 5,width = 10)
+    Cima= Button(janela, text="↑", command= trocaUp)
+    Cima.place(x=360,y=100)
+    Cima.configure(height = 5,width = 10)
 
-Esquerda= Button(janela, text="←", command= trocaLeft)
-Esquerda.place(x=100,y=360)
-Esquerda.configure(height = 5,width = 10)
+    Esquerda= Button(janela, text="←", command= trocaLeft)
+    Esquerda.place(x=100,y=360)
+    Esquerda.configure(height = 5,width = 10)
 
-Direita= Button(janela, text="→", command= trocaRight)
-Direita.place(x=600,y=360)
-Direita.configure(height = 5,width = 10)
+    Direita= Button(janela, text="→", command= trocaRight)
+    Direita.place(x=600,y=360)
+    Direita.configure(height = 5,width = 10)
 
-Baixo= Button(janela, text=" ↓", command= trocaDown)
-Baixo.place(x=360,y=600)
-Baixo.configure(height = 5,width = 10)
+    Baixo= Button(janela, text=" ↓", command= trocaDown)
+    Baixo.place(x=360,y=600)
+    Baixo.configure(height = 5,width = 10)
 
-
-
-janela.mainloop()
+    janela.mainloop()
--- main.py
+++ main.py
@@ -1,44 +1,4 @@
 
+import Interface
 
-import time
-from heap import *
-from Tabuleiro import *
-
-solucao = list(range(10))
-
-#entrada = gerar_tabuleiro_aleatorio()
-entrada = [1,0,2,3,4,5,6,7,8,9]
-
-saida = resolve(entrada)
-
-if saida != False:
-        for i in saida:
-            imprimeTabuleiro(i)
-
-def fds():
-    print(entrada)
-
-    heap = []
-    historico = []
-
-    heap.append(Tabuleiro(entrada, 0, []))
-
-    while heap[0].h != 0 or len(heap) == 0 :
-        topo = remove(heap, 0)
-        historico.append(topo)
-
-        topo.sequencia.append(topo.tabuleiro)
-
-        novos = trocas(topo.tabuleiro, topo.zero)
-        
-        for i in novos :
-            if naoExisteEm(i, historico) and naoExisteEm(i, heap):
-                aumentar_chave(heap,len(heap),Tabuleiro(i , topo.g + 1, topo.sequencia))
-
-    if(len(heap) == 0):
-        print("sem solucao")
-    else:
-        for i in heap[0].sequencia :
-            imprimeTabuleiro(i)
-        
-    print(heap[0])
+Interface.telaStart()
